5b255344ced23fc9067fae5fe8d2425a8435130b
Jens Skott <jens.skott@thoughtgears.co.uk>
2025-04-12 09:01:05 +0100

    testing some new things

--- .infrastructure/terraform.tf
+++ .infrastructure/terraform.tf
@@ -1,11 +1,6 @@
 terraform {
   required_version = "> 1.10.0"
 
-  backend "gcs" {
-    bucket = "thoughtgears-terraform-states"
-    prefix = "poc-user-api"
-  }
-
   required_providers {
     google = {
       source  = "hashicorp/google"
